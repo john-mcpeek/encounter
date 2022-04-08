4607a77095fac51bb951af6bca3946ce571b6a4d
john-mcpeek <juan.mcpeek@gmail.com>
2022-04-08 13:35:29 -0400

    blah

---
+++ src/test/java/integration_tests/GoIT.java
@@ -0,0 +1,16 @@
+package integration_tests;
+
+import io.volcanolabs.encounter.client.api.ApiException;
+import io.volcanolabs.encounter.client.api.EncountersApi;
+import org.junit.jupiter.api.Test;
+
+public class GoIT {
+
+	@Test
+	public void go() throws ApiException {
+		System.out.println("FOOOO!!!");
+		EncountersApi w;
+//		var x = l.getEncountersV1WithHttpInfo( "q" );
+//		x.getHeaders();
+	}
+}
